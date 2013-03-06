5f75cf810ff38abe9e2c08e5de4c8ccfa986bb03
ausdim <koronaios@gmail.com>
2013-03-06 16:00:26 +0200

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 132 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#132 SMP PREEMPT Wed Mar 6 14:11:17 EET 2013"
+#define LINUX_COMPILE_BY "ausdim"
+#define LINUX_COMPILE_HOST "dk-"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
